ca27bd13a2388cb7509b179d7b8275de97a7de8e
Andras Serfozo <subztep@gmail.com>
2021-03-30 00:01:27 +0100

    :hear_no_evil:

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import { defineConfig } from "vite"
 // import pugPlugin from "./src/vite-plugin-pug/index.js"
-import pugPlugin from "vite-plugin-pug"
+// @ts-ignore
+import Pug from "vite-plugin-pug/dist/index.cjs"
+import Linker from "vite-plugin-linker"
 
 export default defineConfig({
   plugins: [
-    pugPlugin(
+    Pug(
       {
         // pretty: true,
         doctype: "html",
       },
       {}
     ),
+    Linker({
+      watch: "../vite-plugin-pug/src",
+      exec: "cd ../vite-plugin-pug && npm run build",
+      dist: "../vite-plugin-pug/dist",
+      target: "./node_modules/vite-plugin-pug/dist",
+    }),
   ],
 })
